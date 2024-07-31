a02c63a8ca903eb9c9d01c6510172ab55c15055c
David Space <genarodavid@gmail.com>
2024-07-30 23:55:37 -0400

    added SpeedInsights from vercel

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import MUIThemeProvider from "./MUIThemeProvider.tsx";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <MUIThemeProvider>
+      <SpeedInsights />
       <App />
     </MUIThemeProvider>
   </React.StrictMode>
