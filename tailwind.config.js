f67f7d506910264b3a1663adc49e1500c5e87a17
David Space <genarodavid@gmail.com>
2024-09-23 14:59:15 -0400

    added grey color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,31 @@
 import plugin from "tailwindcss";
+/** @typedef {import('tailwindcss').Config} Config */
 
-/** @type {import('tailwindcss').Config} */
+/** @type {Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      text: "#050315",
-      background: "#fbfbfe",
-      primary: "#474646",
-      secondary: "#b6b6b6",
-      accent: "#141414",
+      grey: {
+        100: "#F8F9FA",
+        200: "#E9ECEF",
+        300: "#DEE2E6",
+        400: "#CED4DA",
+        DEFAULT: "#ADB5BD",
+        600: "#495057",
+        700: "#343A40",
+        800: "#212529",
+        900: "#131618",
+      }
+    },
+    fontSize: {
+      sm: "0.800rem",
+      base: "1rem",
+      xl: "1.250rem",
+      "2xl": "1.563rem",
+      "3xl": "1.954rem",
+      "4xl": "2.442rem",
+      "5xl": "3.053rem",
     },
     extend: {},
   },
